a358f21e31e8482761feb286ab05d9558baac3ac
oinao709 <oinao777@gmail.com>
2020-06-05 23:08:57 +0900

    参考通りにHello worldを出力できた

---
+++ src/main/kotlin/com/example/book_management/HelloController.kt
@@ -0,0 +1,12 @@
+package com.example.book_management
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+    @GetMapping("/")
+    fun hello(): String {
+        return "Hello World"
+    }
+}
